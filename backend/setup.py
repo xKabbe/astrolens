5b065fb426614b6cc8f376ca551e0d006d863668
Steven Karbjinsky <steven.karbjinsky@web.de>
2024-10-15 18:11:38 +0200

    #6 Remove doublebrackets and old project reference

--- backend/setup.py
+++ backend/setup.py
@@ -3,13 +3,13 @@ File: setup.py
 Author: Steven "Kabbe" Karbjinsky
 Description: Setup configuration for the AstroLens backend project.
 
-For more information, see: https://github.com/xKabbe/pyboulder
+For more information, see: https://github.com/xKabbe/astrolens
 """
 from setuptools import setup, find_packages
 
 setup(
     name='astrolens',
-    version="0.1.0",
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.12',
